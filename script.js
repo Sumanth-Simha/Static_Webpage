d45535da6f0272f3a11ea232c811c27f243b5bc7
Sumanth-Simha <rsumanthsimha@gmail.com>
2025-02-27 17:55:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('play-btn').addEventListener('click', function() {
+    window.open('https://playvalorant.com/en-us/?gad_source=1&gclid=CjwKCAiAlPu9BhAjEiwA5NDSA_YtH-X5J1rJ24_475zJ7JjP6wZIZWuxgxIRsRvg5QIUrPN9sTWauxoCN7oQAvD_BwE&gclsrc=aw.ds', '_blank');
+});
